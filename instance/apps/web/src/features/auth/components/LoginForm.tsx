9c17ace9c3e926ccc4b7c53b7939a59119be288f
Tupynambá Lucas <tupynambalucas@gmail.com>
2026-06-24 12:32:06 -0300

    fix: correct typo in placeholder text in LoginForm component

--- instance/apps/web/src/features/auth/components/LoginForm.tsx
+++ instance/apps/web/src/features/auth/components/LoginForm.tsx
@@ -21,7 +21,7 @@ export const LoginForm = ({ data, errors, onChange, inputRefs, disabled }: Login
         <input
           ref={identifier}
           type="text"
-          placeholder="Usuári ou E-mail"
+          placeholder="Usuário ou E-mail"
           value={data.identifier}
           onChange={(e) => onChange('identifier', e.target.value)}
           className={
