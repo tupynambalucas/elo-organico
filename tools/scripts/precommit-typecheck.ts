857dfe01a6ae51af3eeda990ec2560a9089054b7
Tupynambá Lucas <tupynambalucas@gmail.com>
2026-06-24 12:30:42 -0300

    fix: update precommit typecheck to include portal files in filters

--- tools/scripts/precommit-typecheck.ts
+++ tools/scripts/precommit-typecheck.ts
@@ -55,7 +55,7 @@ for (const file of stagedFiles) {
   } else if (rootDir === 'knowledge-base') {
     filters.add('--filter=@elo-organico/knowledge-base');
   } else if (rootDir === 'portal') {
-    continue;
+    filters.add('--filter=@elo-portal/*');
   } else {
     runAll = true;
     break;
